389903417c4855050ccdce4e09527b5658e86658
Victor Callegari <agro.callegari@hotmail.com>
2024-01-29 09:33:50 -0300

    Ajuste no card perfil

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/eliel/Documentos/projetos/HackathonOrangeJuice/frontend/src/app/layout.tsx
+// File: C:\Users\VICTOR CALLEGARI\Desktop\HackathonOrangeJuice\HackathonOrangeJuice\frontend\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- frontend/src/assets/components/perfilCard/perfilCard.tsx
+++ frontend/src/assets/components/perfilCard/perfilCard.tsx
@@ -4,50 +4,68 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import FormProject from "../formProject/formProject.module";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 import { useState, useEffect } from "react";
 
 export default function MultiActionAreaCard() {
-  const [ fullName, setFullName ] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [local, setlocal] = useState('');
 
-  useEffect(() => {
-      const Storage = JSON.parse(localStorage.getItem("data") || "");
-      console.log(Storage)
-      const data = Storage;
-      const fullName = `${data["first_name"]} ${data["last_name"]}`
-      setFullName(fullName)
-    }, []
-  );
+    useEffect(() => {
+      try {
+          const storedData = localStorage.getItem("data");
+          if (storedData) {
+              const data = JSON.parse(storedData);
+              const fullName = `${data["first_name"]} ${data["last_name"]}`;
+              setFullName(fullName);
+  
+              // Supondo que você tenha uma chave "local" nos seus dados
+              const userLocal = data["local"];
+              setlocal(userLocal);
+          } else {
+              console.error("Dados ausentes no armazenamento local.");
+          }
+      } catch (error) {
+          console.error("Erro ao analisar dados do armazenamento local:", error);
+      }
+  }, []);
 
-  return (
-    <Card elevation={0} sx={{ height: 122,width: 375, maxWidth: 375, display: "flex", gap: 5, padding: 0 }}>
-      <CardActionArea sx={{ width: 122, maxWidth: 122 }}>
-        <CardMedia
-          component="img"
-          style={{
-            width: "122px",
-            height: "122px",
-            borderRadius: "50%",
-          }}
-          image="/Circle.svg"
-          alt="Perfil Usuário"
-        />
-      </CardActionArea>
+    return (
+        <Card elevation={0} sx={{
+            height: 122,
+            width: 375,
+            maxWidth: 375,
+            display: "flex",
+            gap: 5,
+            padding: 0
+        }}>
+            <CardActionArea sx={{ width: 122, maxWidth: 122 }}>
+                <CardMedia
+                    component="img"
+                    style={{
+                        width: "122px",
+                        height: "122px",
+                        borderRadius: "50%",
+                    }}
+                    image="/Circle.svg"
+                    alt="Perfil Usuário"
+                />
+            </CardActionArea>
 
-      <CardContent sx={{ padding: 0}}>
-        <CardContent sx={{ padding: 0, marginBottom: 2}}>
-          <Typography gutterBottom variant="h5" component="div">
-            {fullName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Brasil
-          </Typography>
-        </CardContent>
+            <CardContent sx={{ padding: 0 }}>
+                <CardContent sx={{ padding: 0, marginBottom: 2 }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {fullName}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {local}
+                    </Typography>
+                </CardContent>
 
-        <CardActionArea>
-          <FormProject/>
-        </CardActionArea>
-      </CardContent>
-    </Card>
-  );
+                <CardActionArea>
+                    <FormProject />
+                </CardActionArea>
+            </CardContent>
+        </Card>
+    );
 }
