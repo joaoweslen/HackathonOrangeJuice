166224a6e92cabc8325a6d5ebe77411d1244c36e
joaoweslen <joao.weslen03@gmail.com>
2024-02-04 22:58:03 -0300

    Feat: add validation login | add validation register

--- frontend/src/assets/components/button/button.module.tsx
+++ frontend/src/assets/components/button/button.module.tsx
@@ -1,16 +1,9 @@
 import styles from "./button.module.css";
 
 export default function Button(props:any) {
-    const minCharacterPassword = 6;
-    const regex = /\S+@\S+\.\S+/;
-    const {Password, Email} = props;
     
     return (
         <input 
-            disabled={
-                !(Password.length >= 6 && regex.test(Email))
-            }
-            //onChange = {() => console.log((props.Password).length >= 6,regex.test(props.Email))}
             className={styles.button} 
             {...props}
         />
